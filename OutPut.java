0276171c757a08b593f7efeed060e15cc13b8869
LokeshV790 <lokeshvazirani29@gmail.com>
2024-01-25 14:27:43 +0530

    Java: Basic Output techniques
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

---
+++ OutPut.java
@@ -0,0 +1,20 @@
+public class OutPut {
+    public static void main(String[] args){
+        
+        // Normal Print
+        System.out.print("Line 1");
+        System.out.print("Line 2");
+        //output = Line 1Line 2
+
+        //println
+        System.out.println("Line 1");
+        System.out.print("Line 2");
+        //Output = Line 1
+        //         Line 2
+
+        //"\n"
+        System.out.print("Line 1\nLine 2");
+        //Output = Line 1
+        //         Line 2
+    }
+}
