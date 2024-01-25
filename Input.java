80e11acb77c14c1c4a04ae255f0d913d389e39b9
LokeshV790 <lokeshvazirani29@gmail.com>
2024-01-25 15:14:51 +0530

    Java: Inputs in Java using scanner
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

---
+++ Input.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) {
+        // Input 
+        Scanner sc = new Scanner(System.in);
+        // String name1 = sc.next(); // Input = Lokesh Vazirani
+        // System.out.println(name1);
+        //Output = lokesh
+
+        // String name2 = sc.nextLine(System.in);
+        // System.out.println(name2);
+
+        // for int input
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+        // for float input
+        // float af = sc.nextFloat();
+        // float bf = sc.nextFloat();
+        // float area = 2*(af + bf);
+        // System.out.println(area);        
+    }
+}
