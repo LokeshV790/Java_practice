0c68e70cefd48fbdefef0714d271eae732630dcf
LokeshV790 <lokeshvazirani29@gmail.com>
2024-01-26 21:47:27 +0530

    Java: Loops: For Loop
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

---
+++ Basics/ForLoop.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // counter + 1 = counter++
+        for(int counter = 0; counter < 21; counter++){
+            System.out.println(counter);
+        }
+    }
+}
